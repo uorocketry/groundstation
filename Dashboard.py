23416b1add602d7d8159e80a3abc9b548d816ed7
anthony-talevi <anthony.talevi@gmail.com>
2017-06-14 00:20:22 -0400

    Took all night but added the clock

--- Dashboard.py
+++ Dashboard.py
@@ -1,11 +1,13 @@
 import kivy
 kivy.require("1.10.0")
 
-import datetime
+import time
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
+from kivy.clock import Clock
 
 #Set window not resizeable
 #from kivy.config import Config
@@ -16,14 +18,17 @@ from kivy.core.window import Window
 Window.size = (1920, 1080)
 
 class DashboardGridLayout(GridLayout):
-    def update(self):
-        return datetime.datetime.now()
+
+    def update_clock(self, dt):
+
+        self.display.text = time.asctime()
 
 
 class DashboardApp(App):
-
     def build(self):
-        return DashboardGridLayout()
+        d = DashboardGridLayout()
+        Clock.schedule_interval(d.update_clock, 1)
+        return d
 
 dbApp = DashboardApp()
 dbApp.run()
