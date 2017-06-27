eb80ae32fd4dfd5fd190a91cd866e8832f80cd15
anthony-talevi <anthony.talevi@gmail.com>
2017-06-26 21:37:29 -0400

    Now displays altitude, velocity, acceleration

--- Dashboard.py
+++ Dashboard.py
@@ -22,7 +22,7 @@ from kivy.uix.popup import Popup
 class DashboardGridLayout(GridLayout):
 
     def update_clock(self, dt):
-        self.display.text = time.asctime()
+        self.clck.text = time.asctime()
 
     #temporary to account for Clock sending dt arguments
     #In the future this function will process the data being sent by the rocket
@@ -30,37 +30,48 @@ class DashboardGridLayout(GridLayout):
         self.rocket.update()
 
     def connect(self):
-        #self.ids.graph_alt.add_plot(self.plot)
         self.alt_set = []
         self.vel_set = []
         self.acc_set = []
+        self.time_set = []
         self.rocket = DummyRocket()
         self.rocket.set_thrust(1)
         self.open_popup()
         Clock.schedule_interval(self.update_rocket, 0.001)
-        Clock.schedule_interval(self.read_data, 0.001)
+        Clock.schedule_interval(self.read_data, 0.01)
+        Clock.schedule_interval(self.plot, 0.01)
+
 
     def read_data(self, dt):
-        alt = self.rocket.altitude
-        print(alt)
-        self.alt_set.append(alt)
+        self.alt = self.rocket.altitude
+        self.alt_set.append(self.alt)
 
-        vel = self.rocket.velocity
-        print(vel)
-        self.vel_set.append(vel)
+        self.vel = self.rocket.velocity
+        self.vel_set.append(self.vel)
 
-        acc = self.rocket.acceleration
-        print(acc)
-        self.acc_set.append(acc)
+
+        self.acc = self.rocket.acceleration
+        self.acc_set.append(self.acc)
+
+        self.tim = self.rocket.flight_time
+        self.time_set.append(self.tim)
+
 
     def open_popup(self):
         connection_popup = CustomPopup()
         connection_popup.open()
 
-    def plot(self):
+    def plot(self, dt):
+        self.alt_display.text = str(round(self.alt, 2))
+        self.vel_display.text = str(round(self.vel, 2))
+        self.acc_display.text = str(round(self.acc, 2))
         plot = MeshLinePlot(color=[1, 0, 0, 1])
-        plot.points = [(x, sin(x/10)) for x in range(0, 100)]
+        plot.points = [(x, x) for x in self.alt_set]
+        #self.graph_alt.xmin = self.alt_count + 10
         self.graph_alt.add_plot(plot)
+
+
+
 
 class CustomPopup(Popup):
     pass
@@ -73,7 +84,6 @@ class CustGraph(Graph):
 class DashboardApp(App):
     def build(self):
         d = DashboardGridLayout()
-        d.plot()
         Clock.schedule_interval(d.update_clock, 1)
         return d
 
