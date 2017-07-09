450e5ef0d22cdb1f7f8ad8195b5ef475dc9565c7
anthony-talevi <anthony.talevi@gmail.com>
2017-07-09 18:36:47 -0400

    Made some tweaks to update times

--- Dashboard.py
+++ Dashboard.py
@@ -38,41 +38,54 @@ class DashboardGridLayout(GridLayout):
         self.rocket = DummyRocket()
         self.rocket.set_thrust(1)
         self.open_popup()
+        self.plot()
+
         Clock.schedule_interval(self.update_rocket, 0.001)
-        Clock.schedule_interval(self.read_data, 0.001)
-        Clock.schedule_interval(self.plot, 0.001)
-
-
-    def read_data(self, dt):
-        self.ele += 1
-        self.alt = self.rocket.altitude
-        self.alt_set.append(self.alt)
-
-        self.vel = self.rocket.velocity
-        self.vel_set.append(self.vel)
-
-
-        self.acc = self.rocket.acceleration
-        self.acc_set.append(self.acc)
-
-        self.tim = self.rocket.flight_time
-        self.tim_set.append(self.tim)
-
+        Clock.schedule_interval(self.update, 1)
 
     def open_popup(self):
         connection_popup = CustomPopup()
         connection_popup.open()
 
-    def plot(self, dt):
+    def plot(self):
+        self.plot_alt = MeshLinePlot(color=[1, 0, 0, 1])
+        self.plot_alt.points = [(0,0)]
+        self.graph_alt.add_plot(self.plot_alt)
+
+        self.plot_vel = MeshLinePlot(color=[1,0,0,1])
+        self.plot_vel.points = [(0,0)]
+        self.graph_vel.add_plot(self.plot_vel)
+
+        self.plot_acc = MeshLinePlot(color=[1,0,0, 1])
+        self.plot_acc.points = [(0,0)]
+        self.graph_acc.add_plot(self.plot_acc)
+
+
+    def update(self, dt):
+        self.alt = self.rocket.altitude
+        #self.alt_set.append(self.alt)
+
+        self.vel = self.rocket.velocity
+        #self.vel_set.append(self.vel)
+
+        self.acc = self.rocket.acceleration
+        #self.acc_set.append(self.acc)
+
+        self.tim = self.rocket.flight_time
+        #self.tim_set.append(self.tim)
+
         self.alt_display.text = str(round(self.alt, 2))
         self.vel_display.text = str(round(self.vel, 2))
         self.acc_display.text = str(round(self.acc, 2))
-        plot = MeshLinePlot(color=[1, 0, 0, 1])
-        plot.points = [(x/1000, self.alt_set[x]) for x in range(self.ele)]
-        #self.graph_alt.xmin = self.alt_count + 10
-        self.graph_alt.add_plot(plot)
 
+        self.plot_alt.points.append((self.tim/1000, self.alt))
+        #= [(x / 1000, self.alt_set[x]) for x in range(self.ele)]
+        self.plot_vel.points.append((self.tim/1000, self.vel))
+        #= [(x / 1000, self.vel_set[x]) for x in range(self.ele)]
+        self.plot_acc.points.append((self.tim/1000, self.acc))
+        #= [(x / 1000, self.acc_set[x]) for x in range(self.ele)]
 
+        self.ele += 1
 
 
 class CustomPopup(Popup):
